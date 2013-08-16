8b1df2d548c62b185356075a284cb2b20f1185d4
morrillo <gustavo.orrillo@moldeo.coop>
2013-08-16 19:24:47 -0300

    Closes #3

--- account_voucher.py
+++ account_voucher.py
@@ -40,30 +40,31 @@ class account_voucher(osv.osv):
         move_pool = self.pool.get('account.move')
         move_line_pool = self.pool.get('account.move.line')
         for voucher in self.browse(cr, uid, ids, context=context):
-            company_currency = self._get_company_currency(cr, uid, voucher.id, context)
-            current_currency = self._get_current_currency(cr, uid, voucher.id, context)
-            # we select the context to use accordingly if it's a multicurrency case or not
-            context = self._sel_context(cr, uid, voucher.id, context)
-            # But for the operations made by _convert_amount, we always need to give the date in the context
-            ctx = context.copy()
-            ctx.update({'date': voucher.date})
-            # Create the account move record.
-            move_id = move_pool.create(cr, uid, self.account_move_get(cr, uid, voucher.id, context=context), context=context)
-            # Get the name of the account_move just created
-            name = move_pool.browse(cr, uid, move_id, context=context).name
-	    for retention in self.pool.get('payment.retention').browse(cr,uid,voucher.retention_ids,context=context):
-		    dict_move_line = self.first_move_line_get(cr,uid,voucher.id, move_id, company_currency, current_currency, context)	
-		    if voucher.type == 'sale':
-		    	account_id = retention.id.tax_id.account_paid_id.id
-			dict_move_line['credit'] = 0
-			dict_move_line['debit'] = retention.id.amount
-		    else:
-		    	account_id = retention.id.tax_id.account_collected_id.id
-			dict_move_line['credit'] = retention.id.amount
-			dict_move_line['debit'] = 0
-		    dict_move_line['account_id'] = account_id
-		    move_line_id = move_line_pool.create(cr,uid,dict_move_line,context)
-	            # Create the second line of the retention
+	        if voucher.retention_ids:
+	            company_currency = self._get_company_currency(cr, uid, voucher.id, context)
+        	    current_currency = self._get_current_currency(cr, uid, voucher.id, context)
+	            # we select the context to use accordingly if it's a multicurrency case or not
+        	    context = self._sel_context(cr, uid, voucher.id, context)
+	            # But for the operations made by _convert_amount, we always need to give the date in the context
+        	    ctx = context.copy()
+	            ctx.update({'date': voucher.date})
+        	    # Create the account move record.
+	            move_id = move_pool.create(cr, uid, self.account_move_get(cr, uid, voucher.id, context=context), context=context)
+        	    # Get the name of the account_move just created
+	            name = move_pool.browse(cr, uid, move_id, context=context).name
+		    for retention in self.pool.get('payment.retention').browse(cr,uid,voucher.retention_ids,context=context):
+			    dict_move_line = self.first_move_line_get(cr,uid,voucher.id, move_id, company_currency, current_currency, context)	
+			    if voucher.type == 'sale':
+			    	account_id = retention.id.tax_id.account_paid_id.id
+				dict_move_line['credit'] = 0
+				dict_move_line['debit'] = retention.id.amount
+			    else:
+		    		account_id = retention.id.tax_id.account_collected_id.id
+				dict_move_line['credit'] = retention.id.amount
+				dict_move_line['debit'] = 0
+			    dict_move_line['account_id'] = account_id
+			    move_line_id = move_line_pool.create(cr,uid,dict_move_line,context)
+		            # Create the second line of the retention
 		    if voucher.type == 'sale':
 		   	    account_id = voucher.partner_id.property_account_receivable.id	     
 			    dict_move_line['credit'] = retention.id.amount
